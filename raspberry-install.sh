6e54f532f748ac553dd7ccdfce697d1b402f0161
ivanc5po <59787536+ivanc5po@users.noreply.github.com>
2022-03-17 20:29:44 +0800

    Update raspberry-install.sh

--- raspberry-install.sh
+++ raspberry-install.sh
@@ -1,3 +1,4 @@
+sudo apt-get install -y automake autoconf pkg-config libcurl4-openssl-dev libjansson-dev libssl-dev libgmp-dev zlib1g-dev make g++ git
 git clone https://github.com/tpruvot/cpuminer-multi.git
 cd cpuminer-multi
 git checkout tags/v1.3.5-multi
