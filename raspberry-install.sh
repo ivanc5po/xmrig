1344af53537516a5f539ecfeb4f7ca5a54b3b225
ivanc5po <59787536+ivanc5po@users.noreply.github.com>
2022-03-17 19:24:32 +0800

    Create raspberry-install.sh

---
+++ raspberry-install.sh
@@ -0,0 +1,5 @@
+git clone https://github.com/tpruvot/cpuminer-multi.git
+cd cpuminer-multi
+git checkout tags/v1.3.5-multi
+./build.sh
+sudo ./cpuminer -o stratum+tcp://us.litecoinpool.org:3333 -u chong0312.1 -p 1
