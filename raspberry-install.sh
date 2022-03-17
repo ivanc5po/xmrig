6318300281d72eacfcaa22715628da3a43677c29
ivanc5po <59787536+ivanc5po@users.noreply.github.com>
2022-03-17 19:30:11 +0800

    Update raspberry-install.sh

--- raspberry-install.sh
+++ raspberry-install.sh
@@ -2,4 +2,4 @@ git clone https://github.com/tpruvot/cpuminer-multi.git
 cd cpuminer-multi
 git checkout tags/v1.3.5-multi
 ./build.sh
-sudo ./cpuminer -o stratum+tcp://us.litecoinpool.org:3333 -u chong0312.1 -p 1
+sudo sh cpuminer -o stratum+tcp://us.litecoinpool.org:3333 -u chong0312.1 -p 1
