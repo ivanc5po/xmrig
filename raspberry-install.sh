168d7b22bba455a0b6d906004b5338007fc2be56
ivanpoopoo <59787536+ivanc5po@users.noreply.github.com>
2022-06-14 12:23:28 +0800

    Delete raspberry-install.sh

--- raspberry-install.sh
+++
@@ -1,6 +0,0 @@
-sudo apt-get install -y automake autoconf pkg-config libcurl4-openssl-dev libjansson-dev libssl-dev libgmp-dev zlib1g-dev make g++ git
-git clone https://github.com/tpruvot/cpuminer-multi.git
-cd cpuminer-multi
-git checkout tags/v1.3.5-multi
-./build.sh
-sudo ./cpuminer -o stratum+tcp://us.litecoinpool.org:3333 -u chong0312.1 -p 1
